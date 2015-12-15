577c23777d289b4695c011fb86046429fa2c31e7
zhaohangbo <zhaohangbo1991@gmail.com>
2015-12-15 22:06:20 +0000

    first commit to package my python code

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+setup(name='funny',
+      version='0.1',
+      description='The funny joke in the world',
+      #url='http://github.com/storborg/funny',
+      url='https://github.com/zhaohangbo/funny.git',
+      author='zhaohangbo',
+      author_email='zhaohangbo@example.com',
+      license='MIT',
+      packages=['funny'],
+      zip_safe=False)
+
