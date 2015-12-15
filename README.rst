24bc648f7f85928dbc4c01a3852433d3b3af62c9
zhaohangbo <zhaohangbo1991@gmail.com>
2015-12-15 22:34:10 +0000

    after python setup.py sdist

---
+++ README.rst
@@ -0,0 +1,7 @@
+pyfun
+--------
+
+To use (with caution), simply do::
+
+    >>> import pyfun
+    >>> print pyfun.joke()
---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='pyfun',
+      version='0.1',
+      description='The pyfun joke in the world',
+      url='http://github.com/storborg/pyfun',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['pyfun'],
+      zip_safe=False)
